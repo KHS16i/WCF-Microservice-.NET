9c26973ce2c15da9cdef093f9bd8feb2149da358
khs16 <khs16@KHS16-PC>
2024-07-13 14:57:45 -0600

    Agregar archivos de proyecto.

---
+++ WS/WSPersonas.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WS
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class WSPersonas : IWSPersonas
+    {
+        public Persona ObtenerPersona(string ID)
+        {
+            if (ID == "0")
+            {
+                return new Persona() { ID = "0", Name = "Kevin", Description = "Fullstack programmer", topSecret = "KHS16" };
+            }
+            else if (ID == "1")
+            {
+                return new Persona() { ID = "1", Name = "Josue", Description = "BDA", topSecret = "KHS16i" };
+            }
+            else
+            {
+                return new Persona() { Error = "Persona no encontrada" };
+            }
+        }
+    }
+}
---
+++ WS1/IWSPersonas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WS1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IWSPersonas
+    {
+        [OperationContract]
+        Persona ObtenerPersona(string ID);
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class Persona : BaseResponse
+    {
+        [DataMember]
+        public string ID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        public string topSecret { get; set; }
+
+    }
+
+    [DataContract]
+    public class BaseResponse
+    {
+        [DataMember]
+        public string Msj { get; set; }
+
+        [DataMember]
+        public string Error { get; set; }
+    }
+}
